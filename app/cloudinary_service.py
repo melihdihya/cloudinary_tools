804ae399a8528f238319f65f146543ec96c44dfd
melih-avci <melih-avci@outlook.de>
2024-10-13 15:30:02 +0200

    Add: Provide function to update context on existing assets

--- app/cloudinary_service.py
+++ app/cloudinary_service.py
@@ -4,10 +4,13 @@ import re
 from fractions import Fraction
 import os
 import io
+import cloudinary.api
+import cloudinary.search
+import cloudinary.uploader
+from loguru import logger
 from PIL import Image, ImageFile
 from PIL.ExifTags import TAGS
 import cloudinary
-import cloudinary.uploader
 import dotenv
 
 MAX_FILE_SIZE = 10_485_760
@@ -58,18 +61,26 @@ def convert_shutter_speed(value: float) -> str:
         return f"{int(value)}s"
 
 
-def clean_software_tag(text: str) -> str:
+def clean_software_tag(exif_dict: str) -> dict:
     """Removes unnecessary version information from the software tag"""
-    if "Adobe" in text:
-        match = re.search(r"Lightroom Classic \d+\.\d+", text)
+    software_version = None
+    if "Adobe" in exif_dict["Software"]:
+        match = re.search(r"Lightroom Classic \d+\.\d+", exif_dict["Software"])
         if match:
-            return match.group(0)
+            software_version = match.group(0)
     else:
-        match = re.search(r"Ver\.? ?(\d+\.\d+)|Ver(\d+\.\d+)", text)
+        match = re.search(r"Ver\.? ?(\d+\.\d+)|Ver(\d+\.\d+)", exif_dict["Software"])
         if match:
             version_number = match.group(1) or match.group(2)
-            return str(float(version_number))
-    return text
+            software_version = str(float(version_number))
+
+    if "Lightroom" in software_version:
+        exif_dict["Software"] = software_version
+    else:
+        exif_dict["Software"] = (
+            f"{exif_dict["Model"].replace("NIKON ", "")} Ver. {software_version}"
+        )
+    return exif_dict
 
 
 def get_image_exif(image: ImageFile) -> dict:
@@ -106,13 +117,10 @@ def filter_exif(exif: dict) -> dict:
     exif_clean["ExposureTime"] = convert_shutter_speed(
         float(exif_clean["ExposureTime"])
     )
-    software_version = clean_software_tag(exif_clean["Software"])
-    if "Lightroom" in software_version:
-        exif_clean["Software"] = software_version
-    else:
-        exif_clean["Software"] = (
-            f"{exif_clean["Model"].replace("NIKON ", "")} Ver. {software_version}"
-        )
+    exif_clean = clean_software_tag(exif_clean)
+    exif_clean["ISO"] = exif_clean.pop("ISOSpeedRatings")
+    exif_clean["FocalLengthIn35mmFormat"] = exif_clean.pop("FocalLengthIn35mmFilm")
+
     return exif_clean
 
 
@@ -166,24 +174,53 @@ def upload_image(
     )
 
 
-def get_all_images(folder: str) -> dict:
+def get_cdn_images(folder: str) -> dict:
     """Get all images in a specified folder"""
-
-    return cloudinary.search.resources(
-        type="upload", expression=f"folder:{folder}", max_results=500
+    results = (
+        cloudinary.search.Search()
+        .expression(f"folder:{folder}")
+        .sort_by("public_id", "asc")
+        .max_results(400)
+        .execute()
     )
+    return results
 
 
-def update_image(public_id: str, context: dict) -> dict:
+def get_cdn_image(public_id: str) -> dict:
+    """Get image from cloudinary"""
+    return cloudinary.api.resource(public_id, image_metadata=True)
+
+
+def extract_exif_from_cdn_image(image: dict) -> dict:
+    """Extract exif data from cloudinary image"""
+    exif_clean = {
+        "Model": image["image_metadata"]["Model"],
+        "LensModel": image["image_metadata"]["LensModel"],
+        "FocalLengthIn35mmFormat": image["image_metadata"][
+            "FocalLengthIn35mmFormat"
+        ].replace(" ", ""),
+        "FNumber": image["image_metadata"]["FNumber"],
+        "ExposureTime": f"{image["image_metadata"]["ExposureTime"]}s",
+        "ISO": image["image_metadata"]["ISO"],
+        "ExposureProgram": image["image_metadata"]["ExposureProgram"],
+        "Software": image["image_metadata"]["Software"],
+        "DateTimeOriginal": image["image_metadata"]["DateTimeOriginal"],
+    }
+    exif_clean = clean_software_tag(exif_clean)
+    return exif_clean
+
+
+def update_cdn_image(public_id: str, context: dict) -> dict:
     """Update image context in cloudinary"""
     context_str = "|".join(f"{key}={value}" for key, value in context.items())
-    return cloudinary.uploader.explicit(public_id, context=context_str)
+    return cloudinary.uploader.explicit(public_id, type="upload", context=context_str)
 
 
-def upload_folder(folder_path: str):
+def upload_folder(folder_path: str, cdn_folder: str = None):
     """Upload all images in a folder to cloudinary"""
     image_files = get_image_files(folder_path)
-    cdn_folder = os.path.basename(folder_path)
+    if not cdn_folder:
+        cdn_folder = os.path.basename(folder_path)
 
     for image_path in image_files:
         public_id = os.path.basename(image_path)
@@ -193,7 +230,23 @@ def upload_folder(folder_path: str):
         context = filter_exif(exif)
         compressed_image = reduce_image_size(image)
         converted_image = image_to_bytesio(compressed_image)
+        logger.info(f"Uploading {public_id} to Cloudinary")
         upload_image(converted_image, cdn_folder, public_id, context)
+        logger.info(f"Uploaded {public_id} to Cloudinary")
 
 
+def update_cdn_folder(folder: str):
+    """Add context to all images in a folder"""
+    images = get_cdn_images(folder)
+    for image in images["resources"]:
+        logger.info(f"Updating {image['public_id']} in Cloudinary")
+        public_id = image["public_id"]
+        image_details = get_cdn_image(public_id)
+        context = extract_exif_from_cdn_image(image_details)
+        update_cdn_image(public_id, context)
+        logger.info(f"Updated {public_id} in Cloudinary")
+
+
+# Example usage:
 # upload_folder("/Users/melihavci/Desktop/Website/Test_Auto_Upload")
+# update_cdn_folder("Testing")
